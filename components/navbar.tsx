5aa457e5fa451fdec272d6328a7e3a908ca1ffca
yijiu <yijiubd@gmail.com>
2023-07-07 18:46:30 +0800

    feat:添加页面布局测试

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,25 +3,33 @@ import styles from './navbar.module.css'
 const Navbar = () => {
 
     return (
-        <nav>
-            <ul className={styles.ul}>
-                <li>
-                    <Link href="/">
-                        Home
-                    </Link>
-                </li>
-                <li>
-                    <Link href="/about">
-                        About
-                    </Link>
-                </li>
-                <li>
-                    <Link href="/contact">
-                        Contact
-                    </Link>
-                </li>
-            </ul>
-        </nav>
+        <div className={styles.container}>
+            <div className={styles.name}><text>Yi Jiu </text></div>
+            <div className={styles.navbar}>
+                <nav>
+                    <ul className={styles.ul}>
+                        <li>
+                            <Link href="#home">
+                                Home
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href="#about">
+                                About
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href="#contact">
+                                Contact
+                            </Link>
+                        </li>
+                    </ul>
+                </nav></div>
+            <div className={styles.home} id='home'></div>
+            <div className={styles.about} id='about'></div>
+            <div className={styles.contact} id='contact'></div>
+        </div>
+
     );
 };
 
