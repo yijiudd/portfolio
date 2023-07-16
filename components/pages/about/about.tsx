c53b8d8f96c18596724911617a7fd152d5ed81af
yijiu <yijiubd@gmail.com>
2023-07-16 19:32:08 +0800

    feat:add animation

--- components/pages/about/about.tsx
+++ components/pages/about/about.tsx
@@ -1,33 +1,44 @@
+'use client'
 import Image from 'next/image'
 import styles from '../about/about.module.css'
+import { useState } from 'react'
 const About = () => {
-    return <div className={styles.about} id='about'>
-        <div className={styles.contentArea} >
-            <div className={styles.introText}><p>Let me introduce myself:</p></div>
-            <h3 className={styles.helloText} >Hi! I am Yi Jiu</h3>
-            <p className={styles.intro}> I worked at ByteDance as a Front-end Engineer developing H5 games before,but now I am a CS graducate student in Waseda University. I am doing research on computer vision.I dream about working in game industry in Japan one day. </p>
-            <div className={styles.Area}>
-                <div className={styles.name}>
-                    <text>Name:</text>
-                    <text>Yi Jiu</text>
-                </div>
-                <div className={styles.name}>
-                    <text>Personality:</text>
-                    <text>INFP</text>
-                </div>
-                <div className={styles.name}>
-                    <text>BrithDay:</text>
-                    <text>1999/9/19</text>
-                </div>
-                <div className={styles.name}>
-                    <text>Hobbies:</text>
-                    <text>BasketBall、Movie、Anime VideoGames</text>
+    // const [hoverTime, setHoverTime] = useState(0)
+    // const handleHover = () => {
+    //     console.log('mouseLive1', hoverTime, styles.contentArea)
+    //     setHoverTime(hoverTime => hoverTime + 1)
+    //     console.log('mouseLive2', hoverTime, styles.contentArea)
+    // }
+
+    return <div className={styles.about} id='about' >
+        <div className={styles.totalArea}>
+            <div className={`${styles.contentArea} ${styles.moveLeft}`} >
+                <div className={styles.introText}><p>Let me introduce myself:</p></div>
+                <h3 className={styles.helloText} >Hi! I am Yi Jiu</h3>
+                <p className={styles.intro}> I worked at ByteDance as a Front-end Engineer developing H5 games before,but now I am a CS graducate student in Waseda University. I am doing research on computer vision.I dream about working in game industry in Japan one day. </p>
+                <div className={styles.Area}>
+                    <div className={styles.name}>
+                        <text>Name:</text>
+                        <text>Yi Jiu</text>
+                    </div>
+                    <div className={styles.name}>
+                        <text>Personality:</text>
+                        <text>INFP</text>
+                    </div>
+                    <div className={styles.name}>
+                        <text>BrithDay:</text>
+                        <text>1999/9/19</text>
+                    </div>
+                    <div className={styles.name}>
+                        <text>Hobbies:</text>
+                        <text>BasketBall、Movie、Anime VideoGames</text>
+                    </div>
                 </div>
             </div>
+            <div className={`${styles.imageArea} ${styles.moveRight}`}>
+                <Image src='/person.jpeg' alt='Personal Picture' width={400} height={844}></Image>
+            </div>
         </div>
-        <div className={styles.imageArea}>
-            <Image src='/person.jpeg' alt='Personal Picture' width={400} height={844}></Image>
-        </div>
-    </div>
+    </div >
 }
 export default About
